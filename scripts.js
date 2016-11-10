643718b33d4d800ce6430600d001a5d77cd7551b
Christina Bergstrom <bergstromcbb@gmail.com>
2016-11-10 16:41:10 -0500

    first round of theatre files

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    var seats = 1
+
+    setupPage();
+
+    function setupPage() {
+        for(var i = 1; i < 25; i++) {
+        var seats = i
+
+        var seatsChart = document.createElement('div');
+
+        seatsChart.innerHTML = seats;
+        
+        document.body.appendChild(seatsChart);    
+            }
+
+        seats++;
+    }
+});
